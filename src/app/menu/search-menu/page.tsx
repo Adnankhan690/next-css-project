60085f9d1107034e31350c50894f39df04252399
Adnankhan690 <adnan73803@gmail.com>
2025-04-26 21:31:00 +0530

    fix input border

--- src/app/menu/search-menu/page.tsx
+++ src/app/menu/search-menu/page.tsx
@@ -90,7 +90,7 @@ export default function SearchMenu () {
 
             Some other text..Some text..Some text..Some text..Some text..Some text..Some text..Some text..
 
-            Some text..
+            Some text.. 
           </p>
 
         </div>
