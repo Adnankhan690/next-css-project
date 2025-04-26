f163a979800540f67ba75d39c73b09e1c4435534
Adnankhan690 <adnan73803@gmail.com>
2025-04-26 20:56:48 +0530

    changes

--- src/app/menu/responsive-topnav/page.tsx
+++ src/app/menu/responsive-topnav/page.tsx
@@ -3,19 +3,21 @@ import { Menu } from 'lucide-react';
 import styles from './responsiveTopnav.module.scss';
 import { useState } from 'react';
 
-export default function ResponsiveTopnav () { 
+export default function ResponsiveTopnav () {
   const [responsive, setResponsive] = useState(false);
   return (
-    <div className={styles.mainCon}>
-      <div className={`${styles.topNavCon} ${responsive ? styles.hide : styles.show}`}>
-        <div className={styles.active}>Home</div>
-        <div >News</div>
-        <div >Contact</div>
-        <div >About</div>
-      </div>
-      <div className={styles.menuCon} onClick={() => {setResponsive(!responsive)}}>
-        <Menu className={styles.menu} />
-      </div>
-    </div>
+    <>
+      <p>Responsive Top Nav [Resize-window to see the effect]</p>
+      <div className={styles.mainCon}>
+        <div className={`${styles.topNavCon} ${responsive ? styles.hide : styles.show}`}>
+          <div className={styles.active}>Home</div>
+          <div >News</div>
+          <div >Contact</div>
+          <div >About</div>
+        </div>
+        <div className={styles.menuCon} onClick={() => { setResponsive(!responsive); }}>
+          <Menu className={styles.menu} />
+        </div>
+      </div></>
   );
 }
